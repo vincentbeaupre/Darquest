e6b19d3613c64e13c0f9469514fec1a9ff0e366e
VincentLajoie <125910689+THzWeeds@users.noreply.github.com>
2023-03-14 14:47:26 -0400

    Cours 1 Layout

---
+++ fonctionJavaScript.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var header = document.getElementById("myHeader");
+  var sticky = header.offsetTop;
+
+  window.onscroll = function() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  };
+});
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <title>Page Principale</title>
+  <link rel="stylesheet" href="stylesheet.css">
+  <script src="fonctionJavaScript.js"></script>
+  <?php require 'fonction.php'; ?>
+</head>
+
+<body>
+
+  <div class="header" id="myHeader">
+    <h2 class="title">DarQuest</h2>
+    <a href="http://167.114.152.54/~darquest6/market.php">
+      <p class="market">Market</p>
+    </a>
+    <a href="http://167.114.152.54/~darquest6/inventaire.php">
+      <p class="inventaire">Inventaire</p>
+    </a>
+    <a href="http://167.114.152.54/~darquest6/quest.php">
+      <p class="quest">Quest</p>
+    </a>
+
+
+
+  </div>
+
+</body>
+
+</html>
