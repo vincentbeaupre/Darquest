d08e86b0e4e6a0aec033a6338f32a0308cde86fe
VincentBeaupre <95039252+vincentbeaupre@users.noreply.github.com>
2023-04-22 13:53:11 -0400

    Select answer

--- logout.php
+++ logout.php
@@ -2,6 +2,6 @@
 session_start();
 session_destroy();
 session_start();
-$_SESSION['message'] = "Vous êtes déconnectés.";
+$_SESSION['message'] = "Vous êtes déconnecté";
 header('location: index.php');
 ?>
--- questions.php
+++ questions.php
@@ -2,32 +2,65 @@
 require_once('bd.php');
 session_start();
 
-(isset($_SESSION['idJoueur'])) ? $idJoueur = $_SESSION['idJoueur'] : "";
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
-  (isset($_POST['difficulte'])) ? $difficulte = $_POST['difficulte'] : "";
-  if($difficulte == 'A'){
-    $question = Database::getQuestionAleatoire($idJoueur);
-  }else{
-    $question = Database::getQuestionDifficulte($idJoueur,$difficulte);
-  }
-  $reponse = Database::getReponses($question['idQuestion']);
+if (isset($_SESSION['idJoueur'])) {
+  $idJoueur = $_SESSION['idJoueur'];
+} else {
+  $_SESSION['message'] = "Connectez-vous pour accéder à Enigma";
+  header('Location: index.php');
+}
 
-}else if($_SERVER['REQUEST_METHOD'] === "GET"){
-    $reponse = Database::getReponses($_GET['idQuestion']);
-    $question = Database::getQuestion($_GET['idQuestion']);
+if ($_SERVER['REQUEST_METHOD'] === "GET") {
+
+  $difficulte = $_GET['difficulte'];
+
+  if ($difficulte == 'A') {
+    $question = Database::getQuestionAleatoire($idJoueur);
+  } else {
+    $question = Database::getQuestionDifficulte($idJoueur, $difficulte);
+  }
+
+  $_SESSION['question'] = $question;
+
+  $reponses = Database::getReponses($question['idQuestion']);
+  $_SESSION['reponses'] = $reponses;
+
+  $_SESSION['answered'] = false;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
+  $question = $_SESSION['question'];
+  $reponses = $_SESSION['reponses'];
+  $reponseJoueur = $_POST['idReponse'];
+  $bonneReponse = Database::getBonneReponse($question['idQuestion']);
+
+  if (!$_SESSION['answered']) {
+
+    if ($reponseJoueur == $bonneReponse) {
+    } else {
+    }
     //! Update les tables (Joueur_Question & soldes de Joueur) selon le return de la fonction de vérification
-    //! Launch le snackbar
+
+    $_SESSION['answered'] = true;
+  } else {
+    $_SESSION['message'] = "Vous ne pouvez répondre qu'une seule fois à chaque question";
+  }
+}
+
+if (isset($_SESSION['message'])) {
+  $message = $_SESSION['message'];
+  unset($_SESSION['message']);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 
-<?php 
-include_once("header.php");
-foreach($reponse as $idReponse){
-}
-?>
+<?php include_once("header.php"); ?>
+
+<?php if (isset($message)) : ?>
+  <div id="snackbar"><?= $message ?></div>
+<?php endif; ?>
 
 <main class="enigmaContainer">
   <div class="row">
@@ -35,46 +68,30 @@ foreach($reponse as $idReponse){
     <div class="col-10"><?= $question['enonce'] ?></div>
     <div class="col-1"></div>
   </div>
-  <div class="row">
-    <div class="col-3"></div>
-    <form action="" method="GET">
-        <input type="hidden" name="idQuestion" value="<?= $question['idQuestion']?>">
-      <input type="hidden" name="idReponse" value="<?= $reponse[0]['idReponse']?>">
-      <button type='submit' class="col-6 btnQuestion"><?php echo $reponse[0]['reponse'] ?>
-      </button>
-    </form>
-    <div class="col-3"></div>
-  </div>
-  <div class="row">
-    <div class="col-3"></div>
-    <form action="" method="GET">
-        <input type="hidden" name="idQuestion" value="<?= $question['idQuestion']?>">
-      <input type="hidden" name="idReponse" value="<?= $reponse[1]['idReponse']?>">
-      <button type='submit' class="col-6 btnQuestion"><?php echo $reponse[1]['reponse'] ?>
-      </button>
-    </form>
-    <div class="col-3"></div>
-  </div>
-  <div class="row">
-    <div class="col-3"></div>
-    <form action="" method="GET">
-        <input type="hidden" name="idQuestion" value="<?= $question['idQuestion']?>">
-      <input type="hidden" name="idReponse" value="<?= $reponse[2]['idReponse']?>">
-      <button type='submit' class="col-6 btnQuestion"><?php echo $reponse[2]['reponse'] ?>
-      </button>
-    </form>
-    <div class="col-3"></div>
-  </div>
-  <div class="row">
-    <div class="col-3"></div>
-    <form action="" method="GET">
-        <input type="hidden" name="idQuestion" value="<?= $question['idQuestion']?>">
-      <input type="hidden" name="idReponse" value="<?= $reponse[3]['idReponse']?>">
-      <button type='submit' class="col-6 btnQuestion"><?php echo $reponse[3]['reponse'] ?>
-      </button>
-    </form>
-    <div class="col-3"></div>
-  </div>
+
+  <?php foreach ($reponses as $reponse) { ?>
+
+    <div class="row">
+      <div class="col-3"></div>
+      <form method="POST">
+
+        <input type="hidden" name="idReponse" value="<?= $reponse['idReponse'] ?>">
+        <button type='submit' class="col-6 btnQuestion" <?php if ($_SESSION['answered']) {echo "disabled";} ?>>
+          <?= $reponse['reponse'] ?>
+          <?php if ($_SERVER['REQUEST_METHOD'] === "POST") { ?>
+            <?php if ($reponse['idReponse'] == $bonneReponse) { ?>
+              <i class="correctAnswer fa fa-check fa-lg"></i>
+            <?php } else if ($reponse['idReponse'] == $reponseJoueur) { ?>
+              <i class="incorrectAnswer fa fa-times fa-lg"></i>
+            <?php } ?>
+          <?php } ?>
+        </button>
+      </form>
+      <div class="col-3"></div>
+    </div>
+
+  <?php } ?>
+
   <div class="row">
     <div class="col-12"></div>
   </div>
@@ -88,6 +105,15 @@ foreach($reponse as $idReponse){
   </div>
 -->
 </main>
+<script>
+  if (document.getElementById("snackbar") != null) {
+    var snackbar = document.getElementById("snackbar");
+    snackbar.classList.add("show");
+    setTimeout(function() {
+      snackbar.classList.remove("show");
+    }, 3000);
+  }
+</script>
 </body>
 
 </html>
